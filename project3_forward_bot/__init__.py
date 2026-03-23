ae0622062ccb48a8d6e4851fbff070e42667ee5f
Ana-Maria Niculae <ana.niculae@ubbcluj.ro>
2026-03-24 00:02:23 +0200

    working on statistics

--- project3_forward_bot/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file marks the folder as a Python package so it can be imported later
--- shared/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file marks the shared folder as a Python package so it can be imported by all projects
